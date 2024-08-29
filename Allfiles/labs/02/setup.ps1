e5e2aa0c2ae33fe6fab1f1adec392b61002a01f1
duniaclark23 <50409069+duniaclark23@users.noreply.github.com>
2024-08-29 13:28:10 -0400

    Update setup.ps1

--- Allfiles/labs/02/setup.ps1
+++ Allfiles/labs/02/setup.ps1
@@ -85,7 +85,7 @@ $resourceGroupName = "dp203-$suffix"
 Write-Host "Finding an available region. This may take several minutes...";
 $delay = 0, 30, 60, 90, 120 | Get-Random
 Start-Sleep -Seconds $delay # random delay to stagger requests from multi-student classes
-$preferred_list = "eastus"
+$preferred_list = "centralus","southcentralus","eastus","westus","westus2"
 $locations = Get-AzLocation | Where-Object {
     $_.Providers -contains "Microsoft.Synapse" -and
     $_.Providers -contains "Microsoft.Sql" -and
